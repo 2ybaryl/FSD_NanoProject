a6a62a42e161313a7d06045e1969b1cbe1786a8a
Naman Khattar <khattarnaman915@gmail.com>
2022-12-10 23:50:06 +0530

    Server running

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express=require("express");
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+app.use(express.static(__dirname+'/public'));
+
+app.get("/hello", (req, res)=>{
+    res.send("Hello World!");
+})
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
